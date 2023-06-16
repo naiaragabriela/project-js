052c906582fabda8e77eed28172a55752d79d2f1
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-16 12:04:01 -0300

    feat: creation exercise objects

--- exercicios/objetos.js
+++ exercicios/objetos.js
@@ -1,18 +1,23 @@
 const pessoa = {
-    nome: "Luiz",
-    sobrenome: "Miranda",
-    idade: 30,
-    endereco: {
-        rua: "Av Brasil",
-        numero: 320
-    }
+  nome: "Luiz",
+  sobrenome: "Miranda",
+  idade: 30,
+  endereco: {
+      rua: "Av Brasil",
+      numero: 320
+  }
 };
 
 
 //Atribuição via desestruturação 
 
-const { nome, sobrenome, idade } = pessoa;
-console.log(nome, sobrenome, idade);
+const { nome, sobrenome } = pessoa;
+console.log(nome, sobrenome);
 
 const { endereco: { rua, numero } } = pessoa;
 console.log(rua, numero);
+
+
+//criar uma váriavel com as outras propriedades
+const { idade, ...resto } = pessoa;
+console.log(resto);
