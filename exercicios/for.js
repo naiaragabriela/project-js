d24f1d04cb9dcf25d4da16db1dfcaeef2bb5453e
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-16 13:38:41 -0300

    feat: using for

---
+++ exercicios/for.js
@@ -0,0 +1,10 @@
+for (let i = 0; i<=5; i++ ) {
+    console.log(i);
+}
+console.log();
+
+const frutas = ['Maçã', 'Pera', 'Uva', 'Banana'];
+
+for (let f = 0; f < frutas.length; f++ ){
+    console.log(`Indice ${f}`, frutas[f]);
+}
