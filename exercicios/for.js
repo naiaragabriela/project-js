742b0f32916e0befeffcb3763363442bb1fef497
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-16 16:08:38 -0300

    feat:  exercicies using for, while and do while

--- exercicios/for.js
+++ exercicios/for.js
@@ -8,3 +8,27 @@ const frutas = ['Maçã', 'Pera', 'Uva', 'Banana'];
 for (let f = 0; f < frutas.length; f++ ){
     console.log(`Indice ${f}`, frutas[f]);
 }
+console.log();
+
+for (let indice in frutas) {
+    console.log(frutas[indice]);
+}
+console.log();
+
+for (let valor of frutas){
+    console.log(valor);
+}
+console.log();
+
+frutas.forEach(function(valor, indice) {
+    console.log(valor, indice);
+});
+console.log();
+
+const pessoa = {
+    nome: 'Naiara',
+    sobrenome: 'Gabriela'
+};
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}
