265c5ad3a1e77ae782e7e1bd282ad4dfbd70e227
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-21 10:10:07 -0300

    feat: create fetch api

--- Promises/XMLHttpRequest/assets/js/main.js
+++ Promises/XMLHttpRequest/assets/js/main.js
@@ -1,13 +1,13 @@
 const request = obj => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true)
+    xhr.open(obj.method, obj.url, true);
     xhr.send();
-    
-    xhr.addEventListener('load', () => { 
-      if (xhr.status <= 200 && xhr.status < 300) {
+
+    xhr.addEventListener('load', () => {
+      if(xhr.status >= 200 && xhr.status < 300) {
         resolve(xhr.responseText);
-      }else {
+      } else {
         reject(xhr.statusText);
       }
     });
@@ -15,30 +15,32 @@ const request = obj => {
 };
 
 document.addEventListener('click', e => {
-    const el = e.target;
-    const tag = el.tagName.toLowerCase();
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
 
-    if(tag === 'a') {
-        e.preventDefault();
-        carregaPagina(el);
-    }
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
 });
 
 async function carregaPagina(el) {
-    const href = el.getAttribute('href');
-    const objConfig = {
-        method: 'GET',
-        url: href
-    };
-    try {
-        const response = await request(objConfig);
-        carregaResultado(response);
-    } catch(e) {
-        console.log(e);
-    }
+  const href = el.getAttribute('href');
+
+  const objConfig = {
+    method: 'GET',
+    url: href
+  };
+
+  try {
+    const response = await request(objConfig);
+    carregaResultado(response);
+  } catch(e) {
+    console.log(e);
+  }
 }
 
-function carregaResultado(response){
-    const resultado = document.querySelector('.resultado');
-    resultado.innerHTML = response; 
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
 }
